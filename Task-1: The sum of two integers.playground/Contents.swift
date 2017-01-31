46c37d5f56036b5d4a5db98205589b3e08b7d719
Uskinn <s.nevzor@yahoo.com>
2017-01-31 11:59:42 -0500

    first task solved

---
+++ Task-1: The sum of two integers.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// read integers line by line
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+
+
+a = 3
+b = 3
+
+print(a + b)
+
+
